ddb4c2919ab2bc0ef62298eacc25bab97e564ba8
Walrussuit101 <tjefferson@ycp.edu>
2022-07-19 14:42:57 -0400

    flesh out build script

---
+++ src/build.ts
@@ -0,0 +1,29 @@
+import { readFileSync, writeFileSync, copyFileSync } from 'fs';
+import path from 'path';
+
+const originalPkgPath = path.join(__dirname, '..', 'package.json');
+const originalReadMePath = path.join(__dirname, '..', 'README.md');
+
+const buildPath = path.join(__dirname, '..', 'dist');
+const buildPkgPath = path.join(buildPath, 'package.json');
+const buildReadMePath = path.join(buildPath, 'README.md');
+
+const main = () => {
+    const originalPkgString = readFileSync(originalPkgPath).toString('utf8');
+    const originalPkgObj = JSON.parse(originalPkgString);
+
+    // update pkg obj
+    originalPkgObj.main = 'src/index.js';
+    delete originalPkgObj.devDependencies;
+    delete originalPkgObj.scripts;
+
+    // write out new pkg file and README
+    writeFileSync(buildPkgPath, JSON.stringify(originalPkgObj, null, 2));
+    copyFileSync(originalReadMePath, buildReadMePath);
+}
+
+try {
+    main();
+} catch(e) {
+    console.error(e);
+}
