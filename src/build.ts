a0a348de317b0a887a0059f398554559f947e975
Walrussuit101 <tjefferson@ycp.edu>
2022-07-21 09:02:05 -0400

    add license copy to build file

--- src/build.ts
+++ src/build.ts
@@ -3,10 +3,12 @@ import path from 'path';
 
 const originalPkgPath = path.join(__dirname, '..', 'package.json');
 const originalReadMePath = path.join(__dirname, '..', 'README.md');
+const originalLicensePath = path.join(__dirname, '..', 'LICENSE');
 
 const buildPath = path.join(__dirname, '..', 'build');
 const buildPkgPath = path.join(buildPath, 'package.json');
 const buildReadMePath = path.join(buildPath, 'README.md');
+const buildLicensePath = path.join(buildPath, 'LICENSE');
 
 const main = () => {
     const originalPkgString = readFileSync(originalPkgPath).toString('utf8');
@@ -21,6 +23,7 @@ const main = () => {
     // write out new pkg file and README
     writeFileSync(buildPkgPath, JSON.stringify(originalPkgObj, null, 2));
     copyFileSync(originalReadMePath, buildReadMePath);
+    copyFileSync(originalLicensePath, buildLicensePath);
 }
 
 try {
