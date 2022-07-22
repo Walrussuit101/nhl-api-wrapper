c675df502f3a01f282cad5d096aed0ef1062f72e
Walrussuit101 <tjefferson@ycp.edu>
2022-07-21 23:24:07 -0400

    progress on updating build process

--- src/build.ts
+++ src/build.ts
@@ -1,5 +1,7 @@
 import { readFileSync, writeFileSync, copyFileSync } from 'fs';
 import path from 'path';
+import { build } from 'esbuild';
+import { Generator } from 'npm-dts';
 
 const originalPkgPath = path.join(__dirname, '..', 'package.json');
 const originalReadMePath = path.join(__dirname, '..', 'README.md');
@@ -10,11 +12,26 @@ const buildPkgPath = path.join(buildPath, 'package.json');
 const buildReadMePath = path.join(buildPath, 'README.md');
 const buildLicensePath = path.join(buildPath, 'LICENSE');
 
-const main = () => {
+const main = async () => {
+    // compile ts to single js file
+    await build({
+        entryPoints: ['src/index.ts'],
+        outdir: 'build',
+        bundle: true,
+        external: ['axios', 'zod']
+    });
+
+    // generate single types declartion file
+    const types = new Generator({
+        entry: 'src/index.ts',
+        output: 'build/index.d.ts'
+    });
+    await types.generate();
+
+    // update pkg obj
     const originalPkgString = readFileSync(originalPkgPath).toString('utf8');
     const originalPkgObj = JSON.parse(originalPkgString);
 
-    // update pkg obj
     originalPkgObj.main = 'index.js';
     delete originalPkgObj.devDependencies;
     delete originalPkgObj.scripts;
